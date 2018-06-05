df243a2bac3c669a88666a165892b726beef95aa
莫大先生 <1149483498@qq.com>
2018-06-05 21:14:21 +0800

    detailheader

---
+++ src/pages/detail/components/list.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<div v-for="(item,index) of list" :key="index">
+			<div class="item-title">
+				<span class="item-title-icon"></span>
+				{{item.title}}
+			</div>		
+			<div v-if="item.children" class="item-children">
+				<detaillist :list="item.children"></detaillist>
+			</div>	
+		</div>
+		
+	</div>
+	
+</template>
+
+<style scoped>
+	.item-title{
+		position: relative;
+		width: 100%;
+		height: .8rem;
+		line-height: .8rem;
+		border-bottom: 1px solid #eee;
+		text-indent: 1rem
+	}
+	.item-title-icon{
+		display: inline-block;
+	    position: absolute;
+	    width: .36rem;
+	    height: .36rem;
+	    top: .26rem;
+	    left: .2rem;
+	    background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+	    margin-right: .1rem;
+	    background-size: .4rem 3rem;
+	}
+	.item-children{
+		padding: 0 .3rem;
+	}
+</style>
+
+<script>
+export default{
+	name:"detaillist",
+	props:{
+		list:Array
+	},
+	data(){
+		return{
+			
+		}
+	}
+}
+</script>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,6 +2,7 @@
 	<div>
 		<detailbanner></detailbanner>	
 		<detailheader></detailheader>
+		<detaillist :list="list"></detaillist>
 		<div class="bottom"></div>	
 	</div>
 </template>
@@ -9,11 +10,39 @@
 <script>
 import detailbanner from "./components/banner.vue"
 import detailheader from "./components/header.vue"
+import detaillist from "./components/list.vue"
 export default{
 	name:"detail",
 	components:{
 		detailbanner,
 		detailheader,
+		detaillist
+	},
+	data(){
+		return{
+			list:[
+				{
+					title:"成人票",
+					children:[
+						{title:"成人三馆联票"},
+						{title:"成人五馆联票"}
+					]
+				},{
+					title:"学生票",
+					children:[
+						{title:"学生三馆联票"},
+						{title:"学生五馆联票"}
+					]
+				},{
+					title:"儿童票",
+					children:[
+						{title:"儿童三馆联票"},
+						{title:"儿童五馆联票"}
+					]
+				}
+				
+			]
+		}
 	}
 }
 </script>
